b3fc64808725f7dc9e4795e5effd6ed6d5c126c7
JOAO VITOR GONCALVES DOS SANTOS <joao.75918@aluno.pr.senac.br>
2024-03-05 17:06:05 -0300

    adicionando arquivos em javascript

---
+++ type-number.js
@@ -0,0 +1,25 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / 
+segundoNumero;
+
+console.log(operacaoMatematica)
+//
+
+// ponto Flutuante
+
+const numeropontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeropontoFlutuante;
+console.log(novaOperacao)
+
+// NaN -> not a number
+
+const alura = "Alura"
+console.log(alura * primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const textol = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "João"
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
